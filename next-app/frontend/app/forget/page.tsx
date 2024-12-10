f3519548a04e9edc37c8ea9e45b80f109355de90
Kei <kei.patheng@gmail.com>
2024-12-10 22:50:46 +0900

    fix import source

---
+++ next-app/frontend/app/forget/page.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Alert, TextField } from "@mui/material";
+
+import TopBar from "../../components/TopBar";
+import useFormValidation from "../../hooks/useFormValidation";
+import { ForgetFormValues } from "../../interfaces/interfaces";
+import { submitPasswordResetEmail } from "../../services/firebase";
+import { forgetValidationSchema } from "../../utils/validationSchema";
+
+const ForgetPasswordPage = () => {
+  const [message, setMessage] = useState("");
+  // フォームのバリデーションチェック
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useFormValidation<ForgetFormValues>(forgetValidationSchema);
+
+  // パスワードリセットメールを送信
+  const onSubmit = async (data: ForgetFormValues) => {
+    await submitPasswordResetEmail(data.email);
+    setMessage("パスワードリセットメールを送信しました。");
+  };
+
+  return (
+    <div>
+      <TopBar />
+      <div className="flex flex-col text-gray-700 items-center justify-center min-h-screen bg-gray-100">
+        <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+          <h1 className="text-2xl font-bold mb-6 text-center">
+            パスワードの再設定
+          </h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="mb-3 mt-3">
+              <TextField
+                type="email"
+                id="email"
+                label="Eメール"
+                {...register("email")}
+                className="w-full p-2 border border-gray-300 rounded mb-1"
+                autoFocus
+              />
+              {errors.email && (
+                <Alert severity="error" className="text-red-600 text-sm mb-2">
+                  {errors.email?.message as React.ReactNode}
+                </Alert>
+              )}
+              <p className="text-green-500">{message}</p>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <button
+                type="submit"
+                className="w-full p-2 text-lg font-bold bg-blue-500 text-white rounded mt-2 hover:bg-blue-600 transition-colors"
+              >
+                送信
+              </button>
+              <p className="mt-4">
+                アカウントをお持ちでない場合は
+                <Link to="/register" className="text-blue-500">
+                  新規登録
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ForgetPasswordPage;
---
+++ next-app/frontend/utils/validationSchema.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+export const loginValidationSchema = z.object({
+  email: z.string().email("Invalid email address").min(1, "Email is required"),
+  password: z.string().min(8, "At least 8 characters long"),
+});
+export type UserAuth = z.infer<typeof loginValidationSchema>;
+
+export const registerValidationSchema = z
+  .object({
+    name: z.string().min(1, "At least 1 characters long"),
+    email: z
+      .string()
+      .email("Invalid email address")
+      .min(1, "Email is required"),
+    password: z.string().min(8, "At least 8 characters long"),
+    confirm: z.string().min(8, "At least 8 characters long"),
+  })
+  .refine((data) => data.password === data.confirm, {
+    message: "Passwords don't match",
+    path: ["confirm"],
+  });
+
+export const forgetValidationSchema = z.object({
+  email: z.string().email("Invalid email address").min(1, "Email is required"),
+});
