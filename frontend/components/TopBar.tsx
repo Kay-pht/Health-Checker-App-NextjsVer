11e7cd3c97c3f6c1ec558cc4938317f052adc63c
Kei <kei.patheng@gmail.com>
2024-12-20 17:20:17 +0900

    change top bar back

--- frontend/components/TopBar.tsx
+++ frontend/components/TopBar.tsx
@@ -6,20 +6,16 @@ import { Avatar } from "@mui/material";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import { useEffect, useRef } from "react";
 
 const TopBar = () => {
   const router = useRouter();
-  const [user, loading] = useAuthState(auth);
+  const [user] = useAuthState(auth);
 
-  const userNameRef = useRef<string | null | undefined>(user?.displayName);
-  const userPhotoURLRef = useRef<string | null | undefined>(user?.photoURL);
+  // const userNameRef = useRef<string | null | undefined>(user?.displayName);
+  // const userPhotoURLRef = useRef<string | null | undefined>(user?.photoURL);
 
-  useEffect(() => {
-    if (loading) return;
-    userNameRef.current = user?.displayName;
-    userPhotoURLRef.current = user?.photoURL;
-  }, [user]);
+  const userName = user?.displayName;
+  const userPhotoURL = user?.photoURL;
 
   const handleLogout = async () => {
     await logOut();
@@ -28,7 +24,7 @@ const TopBar = () => {
 
   return (
     <div className="bg-blue-600 p-2 flex items-center justify-between ">
-      <Link href={userNameRef ? "/questionnaire" : "/"}>
+      <Link href={userName ? "/questionnaire" : "/"}>
         <h2 className="text-white text-xl ml-3 font-extrabold">
           FoodHealth.navi
         </h2>
@@ -37,21 +33,19 @@ const TopBar = () => {
       {user && (
         <div className="flex items-center space-x-2 font-semibold ml-auto">
           <Link
-            href={userNameRef ? "/mypage" : "/login"}
+            href={userName ? "/mypage" : "/login"}
             className="flex items-center space-x-2"
           >
-            {userPhotoURLRef.current ? (
+            {userPhotoURL ? (
               <Image
-                src={userPhotoURLRef.current}
+                src={userPhotoURL}
                 alt=""
                 className="w-6 h-6 rounded-full"
               />
             ) : (
               <Avatar variant="circular" className="w-6 h-6" />
             )}
-            <span className="text-white">
-              {userNameRef.current ? userNameRef.current : "ゲスト"}
-            </span>
+            <span className="text-white">{userName ? userName : "ゲスト"}</span>
           </Link>
           <button
             onClick={handleLogout}
