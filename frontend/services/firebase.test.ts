8d18fa83793b47a7e6669738ead3ab9ce86a9edd
Kei <kei.patheng@gmail.com>
2025-01-19 18:08:46 +0900

    test :implement firebase Auth test

---
+++ frontend/services/firebase.test.ts
@@ -0,0 +1,51 @@
+import { User } from "firebase/auth";
+import { getToken } from "./firebase";
+
+describe("getToken", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  it("should return a token", async () => {
+    const mockUser: Partial<User> = {
+      getIdToken: jest.fn().mockResolvedValue("mockToken"),
+    };
+    const token = await getToken(mockUser as User);
+    expect(token).toBe("mockToken");
+    expect(mockUser.getIdToken).toHaveBeenCalled();
+  });
+
+  it("should throw an error if can't get token", () => {
+    const mockUser: Partial<User> = {
+      getIdToken: jest.fn().mockRejectedValue(new Error("Failed to get token")),
+    };
+    expect(getToken(mockUser as User)).rejects.toThrow("Failed to get token");
+    expect(mockUser.getIdToken).toHaveBeenCalled();
+  });
+});
+
+// describe("logOut", () => {
+//   jest.mock("firebase/auth", () => ({
+//     ...jest.requireActual("firebase/auth"),
+//     signOut: jest.fn(),
+//   }));
+
+//   beforeEach(() => {
+//     jest.clearAllMocks();
+//   });
+
+//   afterAll(() => {
+//     jest.restoreAllMocks();
+//   });
+
+//   it("should log out user", async () => {
+//     const { signOut } = require("firebase/auth");
+//     await signOut(auth);
+//     expect(signOut).toHaveBeenCalled();
+//     // expect(logOut).not.toThrow();
+//   });
+// });
