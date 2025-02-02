e681f7d2394b47a2f64766d10ed73bf189812cee
Kei <kei.patheng@gmail.com>
2025-02-02 14:11:20 +0900

    test(usePageHandler): Add test for page down handler
    This commit adds a test case for the page down handler in the usePageHandler hook.
    It verifies that the pageDownHandler correctly decrements the currentPageNum.

--- frontend/__tests__/hooks/usePageHandler.test.tsx
+++ frontend/__tests__/hooks/usePageHandler.test.tsx
@@ -3,16 +3,28 @@ import { renderHook } from "@testing-library/react";
 import { act } from "react";
 
 describe("usePageHandler", () => {
-  const { result } = renderHook(() => usePageHandler());
+  const e = {
+    preventDefault: jest.fn(),
+  } as unknown as React.MouseEvent<HTMLButtonElement>;
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
   it("should use pageUpHandler correctly", () => {
+    const { result } = renderHook(() => usePageHandler());
     expect(result.current.currentPageNum).toBe(1);
-    const e = {
-      preventDefault: jest.fn(),
-    } as unknown as React.MouseEvent<HTMLButtonElement>;
     act(() => {
       result.current.pageUpHandler(e);
     });
     // pageUpHandler(e);
     expect(result.current.currentPageNum).toBe(2);
+  });
+
+  it("should use pageDownHandler correctly", () => {
+    const { result } = renderHook(() => usePageHandler());
+    expect(result.current.currentPageNum).toBe(1);
+    act(() => {
+      result.current.pageDownHandler(e);
+    });
+    expect(result.current.currentPageNum).toBe(0);
   });
 });
